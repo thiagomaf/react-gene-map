2d2d8d12aed752d2ea17f721a541d7079b166169
thiagomaf <thiago.maf@gmail.com>
2022-02-05 00:49:44 +0000

    3 files modified, 2 added

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App    from "./App";
 import Level0 from "./Level0";
 import Level1 from "./Level1";
 import Level2 from "./Level2";
+import TestTable from "./TestTable";
 
 ReactDOM.render(<App />, document.getElementById("header"));
 
 ReactDOM.render(<Level0 />, document.getElementById("L0"));
 ReactDOM.render(<Level1 />, document.getElementById("L1"));
 ReactDOM.render(<Level2 />, document.getElementById("L2"));
+
+ReactDOM.render(<TestTable />, document.getElementById("footer"));
